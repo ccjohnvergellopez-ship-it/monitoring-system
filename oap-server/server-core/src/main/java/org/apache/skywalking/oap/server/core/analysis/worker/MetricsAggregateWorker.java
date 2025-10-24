b81f1b6533df9f1ae43a6b2d76279e4f0482cde9
john <cc.johnvergelope.z@cvsu.edu.ph>
2025-10-24 12:33:48 +0800

    commit

---
+++ oap-server/server-core/src/main/java/org/apache/skywalking/oap/server/core/analysis/worker/MetricsAggregateWorker.java
@@ -0,0 +1,161 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.apache.skywalking.oap.server.core.analysis.worker;
+
+import java.util.Arrays;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.skywalking.oap.server.core.UnexpectedException;
+import org.apache.skywalking.oap.server.core.analysis.data.MergableBufferedData;
+import org.apache.skywalking.oap.server.core.analysis.metrics.Metrics;
+import org.apache.skywalking.oap.server.core.worker.AbstractWorker;
+import org.apache.skywalking.oap.server.library.datacarrier.DataCarrier;
+import org.apache.skywalking.oap.server.library.datacarrier.buffer.BufferStrategy;
+import org.apache.skywalking.oap.server.library.datacarrier.buffer.QueueBuffer;
+import org.apache.skywalking.oap.server.library.datacarrier.consumer.BulkConsumePool;
+import org.apache.skywalking.oap.server.library.datacarrier.consumer.ConsumerPoolFactory;
+import org.apache.skywalking.oap.server.library.datacarrier.consumer.IConsumer;
+import org.apache.skywalking.oap.server.library.module.ModuleDefineHolder;
+import org.apache.skywalking.oap.server.telemetry.TelemetryModule;
+import org.apache.skywalking.oap.server.telemetry.api.CounterMetrics;
+import org.apache.skywalking.oap.server.telemetry.api.GaugeMetrics;
+import org.apache.skywalking.oap.server.telemetry.api.MetricsCreator;
+import org.apache.skywalking.oap.server.telemetry.api.MetricsTag;
+
+/**
+ * MetricsAggregateWorker provides an in-memory metrics merging capability. This aggregation is called L1 aggregation,
+ * it merges the data just after the receiver analysis. The metrics belonging to the same entity, metrics type and time
+ * bucket, the L1 aggregation will merge them into one metrics object to reduce the unnecessary memory and network
+ * payload.
+ */
+@Slf4j
+public abstract class MetricsAggregateWorker extends AbstractWorker<Metrics> {
+    public final long l1FlushPeriod;
+    private AbstractWorker<Metrics> nextWorker;
+    private final DataCarrier<Metrics> dataCarrier;
+    private final MergableBufferedData<Metrics> mergeDataCache;
+    private CounterMetrics abandonCounter;
+    private CounterMetrics aggregationCounter;
+    private GaugeMetrics queuePercentageGauge;
+    private long lastSendTime = 0;
+    private final int queueTotalSize;
+
+    MetricsAggregateWorker(ModuleDefineHolder moduleDefineHolder,
+                           AbstractWorker<Metrics> nextWorker,
+                           String modelName,
+                           long l1FlushPeriod,
+                           MetricStreamKind kind,
+                           String poolName,
+                           int poolSize,
+                           boolean isSignalDrivenMode,
+                           int queueChannelSize,
+                           int queueBufferSize
+                           ) {
+        super(moduleDefineHolder);
+        this.nextWorker = nextWorker;
+        this.mergeDataCache = new MergableBufferedData();
+        BulkConsumePool.Creator creator = new BulkConsumePool.Creator(poolName, poolSize, 200, isSignalDrivenMode);
+        this.dataCarrier = new DataCarrier<>(
+            "MetricsAggregateWorker." + modelName, poolName, queueChannelSize, queueBufferSize, BufferStrategy.IF_POSSIBLE);
+        try {
+            ConsumerPoolFactory.INSTANCE.createIfAbsent(poolName, creator);
+        } catch (Exception e) {
+            throw new UnexpectedException(e.getMessage(), e);
+        }
+        this.dataCarrier.consume(ConsumerPoolFactory.INSTANCE.get(poolName), new AggregatorConsumer());
+
+        MetricsCreator metricsCreator = moduleDefineHolder.find(TelemetryModule.NAME)
+                                                          .provider()
+                                                          .getService(MetricsCreator.class);
+        abandonCounter = metricsCreator.createCounter(
+            "metrics_aggregator_abandon", "The abandon number of rows received in aggregation.",
+            new MetricsTag.Keys("metricName", "level", "dimensionality"),
+            new MetricsTag.Values(modelName, "1", "minute")
+        );
+        aggregationCounter = metricsCreator.createCounter(
+            "metrics_aggregation", "The number of rows in aggregation.",
+            new MetricsTag.Keys("metricName", "level", "dimensionality"),
+            new MetricsTag.Values(modelName, "1", "minute")
+        );
+        queuePercentageGauge = metricsCreator.createGauge(
+            "metrics_aggregation_queue_used_percentage", "The percentage of queue used in aggregation.",
+            new MetricsTag.Keys("metricName", "level", "kind"),
+            new MetricsTag.Values(modelName, "1", kind.name())
+        );
+        this.l1FlushPeriod = l1FlushPeriod;
+        queueTotalSize = Arrays.stream(dataCarrier.getChannels().getBufferChannels())
+                               .mapToInt(QueueBuffer::getBufferSize)
+                               .sum();
+    }
+
+    /**
+     * MetricsAggregateWorker#in operation does include enqueue only
+     */
+    @Override
+    public void in(Metrics metrics) {
+        if (!dataCarrier.produce(metrics)) {
+            abandonCounter.inc();
+        }
+    }
+
+    /**
+     * Dequeue consuming. According to {@link IConsumer#consume(List)}, this is a serial operation for every work
+     * instance.
+     *
+     * @param metricsList from the queue.
+     */
+    private void onWork(List<Metrics> metricsList) {
+        metricsList.forEach(metrics -> {
+            aggregationCounter.inc();
+            mergeDataCache.accept(metrics);
+        });
+
+        flush();
+    }
+
+    private void flush() {
+        long currentTime = System.currentTimeMillis();
+        if (currentTime - lastSendTime > l1FlushPeriod) {
+            mergeDataCache.read().forEach(
+                data -> {
+                    nextWorker.in(data);
+                }
+            );
+            lastSendTime = currentTime;
+        }
+    }
+
+    protected class AggregatorConsumer implements IConsumer<Metrics> {
+        @Override
+        public void consume(List<Metrics> data) {
+            queuePercentageGauge.setValue(Math.round(100 * (double) data.size() / queueTotalSize));
+            MetricsAggregateWorker.this.onWork(data);
+        }
+
+        @Override
+        public void onError(List<Metrics> data, Throwable t) {
+            log.error(t.getMessage(), t);
+        }
+
+        @Override
+        public void nothingToConsume() {
+            flush();
+        }
+    }
+}
---
+++ oap-server/server-core/src/main/java/org/apache/skywalking/oap/server/core/status/ServerStatusService.java
@@ -0,0 +1,129 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.apache.skywalking.oap.server.core.status;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.apache.skywalking.oap.server.library.module.ApplicationConfiguration;
+import org.apache.skywalking.oap.server.library.module.ModuleManager;
+import org.apache.skywalking.oap.server.library.module.Service;
+import org.apache.skywalking.oap.server.telemetry.TelemetryModule;
+import org.apache.skywalking.oap.server.telemetry.api.MetricsCreator;
+import org.apache.skywalking.oap.server.telemetry.api.MetricsTag;
+
+/**
+ * The server status service provides the indicators for the current server status.
+ * Notice, this should not be treated as a kind of health checker or self telemetry.
+ * For more, this helps modules to be aware of current OAP server status.
+ *
+ * @since 9.4.0
+ */
+@RequiredArgsConstructor
+public class ServerStatusService implements Service {
+    private final ModuleManager manager;
+    @Getter
+    private BootingStatus bootingStatus = new BootingStatus();
+    @Getter
+    private ClusterStatus clusterStatus = new ClusterStatus();
+
+    private List<ServerStatusWatcher> statusWatchers = new CopyOnWriteArrayList<>();
+
+    private List<ApplicationConfiguration.ModuleConfiguration> configurations;
+
+    public void bootedNow(List<ApplicationConfiguration.ModuleConfiguration> configurations, long uptime) {
+        bootingStatus.setBooted(true);
+        bootingStatus.setUptime(uptime);
+        manager.find(TelemetryModule.NAME)
+               .provider()
+               .getService(MetricsCreator.class)
+               .createGauge("uptime", "oap server start up time", MetricsTag.EMPTY_KEY, MetricsTag.EMPTY_VALUE)
+               // Set uptime to second
+               .setValue(uptime / 1000d);
+        this.statusWatchers.forEach(watcher -> watcher.onServerBooted(bootingStatus));
+        this.configurations = configurations;
+    }
+
+    public void rebalancedCluster(long rebalancedTime) {
+        clusterStatus.setRebalancedTime(rebalancedTime);
+        manager.find(TelemetryModule.NAME)
+               .provider()
+               .getService(MetricsCreator.class)
+               .createGauge(
+                   "cluster_rebalanced_time", "oap cluster rebalanced time after scale", MetricsTag.EMPTY_KEY,
+                   MetricsTag.EMPTY_VALUE
+               )
+               .setValue(rebalancedTime / 1000d);
+
+        this.statusWatchers.forEach(watcher -> watcher.onClusterRebalanced(clusterStatus));
+    }
+
+    public void registerWatcher(ServerStatusWatcher watcher) {
+        this.statusWatchers.add(watcher);
+    }
+
+    /**
+     * @return a complete list of booting configurations with effected values.
+     * @since 9.7.0
+     * @since 10.3.0 return ConfigList instead of String, to support raw configurations.
+     */
+    public ConfigList dumpBootingConfigurations(String keywords4MaskingSecretsOfConfig) {
+        ConfigList configList = new ConfigList();
+        if (configurations == null || configurations.isEmpty()) {
+            return configList;
+        }
+        final String[] keywords = keywords4MaskingSecretsOfConfig.split(",");
+        for (ApplicationConfiguration.ModuleConfiguration configuration : configurations) {
+            final String moduleName = configuration.getModuleName();
+            if (configuration.getProviders().size() == 1) {
+                configList.put(moduleName + ".provider", configuration.getProviders().keySet().iterator().next());
+            }
+            configuration.getProviders().forEach(
+                (providerName, providerConfiguration) ->
+                    providerConfiguration.getProperties().forEach(
+                        (key, value) -> {
+                            for (final String keyword : keywords) {
+                                if (key.toString().toLowerCase().contains(keyword.toLowerCase())) {
+                                    value = "******";
+                                }
+                            }
+                            configList.put(moduleName + "." + providerName + "." + key, value.toString());
+                        }
+                    )
+            );
+        }
+        return configList;
+    }
+
+    public static class ConfigList extends HashMap<String, String> {
+        @Override
+        public String toString() {
+            StringBuilder configList = new StringBuilder();
+            for (final var entry : this.entrySet()) {
+                configList.append(entry.getKey())
+                          .append("=")
+                          .append(entry.getValue())
+                          .append("\n");
+            }
+            return configList.toString();
+        }
+    }
+}
